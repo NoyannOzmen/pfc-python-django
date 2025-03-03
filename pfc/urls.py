6ec6cc9864a0c3d07debddfcd636a2bce767a482
NoyannOzmen <noyann.ozmen@oclock.school>
2025-03-03 19:00:32 +0100

    Added and linked all static pages // Setup static folder for imgs&icons // first pass at animals+shelters pages : no models yet

--- pfc/urls.py
+++ pfc/urls.py
@@ -3,5 +3,19 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('a-propos/', views.static_about, name='about'),
+    path('devenir-famille-d-accueil/', views.static_become_foster, name='become_foster'),
+    path('contact/', views.static_contact, name='contact'),
+    path('faq/', views.static_faq, name='faq'),
+    path('rgpd/', views.static_gdpr, name='gdpr'),
+    path('infos-legales/', views.static_legal, name='legal'),
+    path('plan/', views.static_map, name='map'),
+    path('famille/inscription', views.signin_foster, name='foster_signup'),
+    path('association/inscription/', views.signin_shelter, name='shelter_signup'),
+    path('connexion', views.signin_login, name='login'),
+    path('animaux/', views.animal_list, name='animal_list'),
+    path('animaux/<int:animalId>', views.animal_details, name='animal_details'),
+    path('associations/', views.shelter_list, name='shelter_list'),
+    path('associations/<int:shelterId>', views.shelter_details, name='shelter_details'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- pfc/views.py
+++ pfc/views.py
@@ -1,23 +1,92 @@
 from django.http import HttpResponse
 from django.template import loader
-""" from .models import Member """
-
-""" def members(request):
-  mymembers = Member.objects.all().values()
-  template = loader.get_template('all_members.html')
-  context = {
-    'mymembers': mymembers,
-  }
-  return HttpResponse(template.render(context, request))
-  
-def details(request, id):
-  mymember = Member.objects.get(id=id)
-  template = loader.get_template('details.html')
-  context = {
-    'mymember': mymember,
-  }
-  return HttpResponse(template.render(context, request)) """
+from .models import *
   
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+  
+def static_about(request):
+  template = loader.get_template('static_about.html')
+  return HttpResponse(template.render())
+
+def static_become_foster(request):
+  template = loader.get_template('static_become_foster.html')
+  return HttpResponse(template.render())
+
+def static_contact(request):
+  template = loader.get_template('static_contact.html')
+  return HttpResponse(template.render())
+
+def static_faq(request):
+  template = loader.get_template('static_faq.html')
+  return HttpResponse(template.render())
+
+def static_gdpr(request):
+  template = loader.get_template('static_gdpr.html')
+  return HttpResponse(template.render())
+
+def static_legal(request):
+  template = loader.get_template('static_legal.html')
+  return HttpResponse(template.render())
+
+def static_map(request):
+  template = loader.get_template('static_map.html')
+  return HttpResponse(template.render())
+
+
+def signin_foster(request):
+  template = loader.get_template('signin_foster.html')
+  return HttpResponse(template.render())
+
+def signin_shelter(request):
+  template = loader.get_template('signin_shelter.html')
+  return HttpResponse(template.render())
+
+def signin_login(request):
+  template = loader.get_template('signin_login.html')
+  return HttpResponse(template.render())
+
+""" Models needed from here on """
+
+def animal_list(request):
+  animals = Animal.objects.all().values()
+  species = Species.objects.all().values()
+  tags = Tag.objects.all().values()
+  template = loader.get_template('animal_list.html')
+  context = {
+    'animals': animals,
+    'especes': species,
+    'tags': tags
+  }
+  return HttpResponse(template.render(context, request))
+
+def animal_details(request, id):
+  animal = Animal.objects.get(id=id)
+  template = loader.get_template('animal_details.html')
+  context = {
+    'animal': animal,
+  }
+  return HttpResponse(template.render(context, request))
+
+def shelter_list(request):
+  shelters = Shelter.objects.all().values()
+  species = Species.objects.all().values()
+  tags = Tag.objects.all().values()
+  template = loader.get_template('shelter_list.html')
+  context = {
+    'shelters': shelters,
+    'especes': species,
+    'tags': tags
+  }
+  return HttpResponse(template.render(context, request))
+
+def shelter_details(request, id):
+  shelter = Shelter.objects.get(id=id)
+  template = loader.get_template('shelter_details.html')
+  context = {
+    'shelter': shelter,
+  }
+  return HttpResponse(template.render(context, request))
+
+""" Connected routes go here """
